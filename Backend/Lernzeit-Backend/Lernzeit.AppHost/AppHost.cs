4e6fcbe9e57e707ae5851aa849184458aea0ba17
Moritz Vogel <contact@vogel.business>
2026-04-18 17:00:36 +0200

    feat: add authentication with google

--- Backend/Lernzeit-Backend/Lernzeit.AppHost/AppHost.cs
+++ Backend/Lernzeit-Backend/Lernzeit.AppHost/AppHost.cs
@@ -5,17 +5,22 @@ var builder = DistributedApplication.CreateBuilder(args);
 var db = builder
     .AddPostgres("postgres");
 
-var backend = 
-#if DEBUG
-builder
-    .AddProject<Projects.Lernzeit_Backend>("Backend")
-    .WithReference(db);
-#else
-builder.AddDockerfile("Lernzeit-Backend", "../Lernzeit-Backend/Dockerfile");
-#endif
-builder.AddJavaScriptApp(name: "Frontend", appDirectory: "../../../frontend", "start")
+var googleClientId = builder.AddParameter("GoogleClientId", secret: true);
+var googleClientSecret = builder.AddParameter("GoogleClientSecret", secret: true);
+
+var frontend = builder.AddJavaScriptApp(name: "Frontend", appDirectory: "../../../frontend", "start")
     .WithHttpEndpoint(port: 3000, env: "PORT")
     .WithExternalHttpEndpoints()
-    .WithReference(backend)
     .WithEnvironment("BROWSER", "none"); // Prevents CRA from opening browser on start
+
+var backend = builder
+    .AddProject<Projects.Lernzeit_Backend>("Backend")
+    .WithReference(db)
+    .WithEnvironment("Authentication__Google__ClientId", googleClientId)
+    .WithEnvironment("Authentication__Google__ClientSecret", googleClientSecret)
+    .WithEnvironment("FrontendUrl", frontend.GetEndpoint("http"));
+
+frontend.WithReference(backend)
+    .WithEnvironment("REACT_APP_BACKEND_URL", backend.GetEndpoint("https"));
+
 builder.Build().Run();
